db42f9250cca754dc705bdc1d20ee7bead1c3843
IT21282454 <it21282454@my.sliit.lk>
2025-03-14 17:59:23 +0530

    Create ActivityIntro.tsx

---
+++ frontend/app/Components/Child/ActivityIntro.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ActivityIntro = () => {
+  return (
+    <View>
+      <Text>ActivityIntro</Text>
+    </View>
+  )
+}
+
+export default ActivityIntro
