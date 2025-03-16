3684c95cd529496836b1a66de117cc9664d404ad
IT21282454 <it21282454@my.sliit.lk>
2025-03-17 01:27:13 +0530

    Update DepressionLevel.tsx

--- frontend/app/Components/Child/DepressionLevel.tsx
+++ frontend/app/Components/Child/DepressionLevel.tsx
@@ -217,7 +217,7 @@ const styles = StyleSheet.create({
     },
     startButtonText: {
         color: "#fff",
-        fontSize: 16,
+        fontSize: 14,
         fontFamily: "poppinsSemiBold",
     },
 });
