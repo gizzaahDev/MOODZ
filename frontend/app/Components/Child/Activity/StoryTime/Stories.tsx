430fb0d8e72efd17e226d8a906c12edd96330b03
IT21282454 <it21282454@my.sliit.lk>
2025-03-19 11:54:12 +0530

    Update Stories.tsx

--- frontend/app/Components/Child/Activity/StoryTime/Stories.tsx
+++ frontend/app/Components/Child/Activity/StoryTime/Stories.tsx
@@ -8,7 +8,7 @@ const Stories: storyInter[] = [
     {
         id: "ca21",
         title: "Benny and the Owl",
-        video: "https://firebasestorage.googleapis.com/v0/b/testdb-8ea15.firebasestorage.app/o/MOODZ%2FGDS%2Fvideo%204.mp4?alt=media&token=818c7d5c-239e-4908-9b41-2baa1de06ef5",
+        video: "https://firebasestorage.googleapis.com/v0/b/testdb-8ea15.firebasestorage.app/o/MOODZ%2FChild%2F%F0%9F%91%B6%F0%9F%8F%BB%20Sleep%20Soundly%20with%20Benny%20and%20the%20Owl%20%F0%9F%A6%89%20A%20Bedtime%20Story%20for%20Babies%20and%20Toddlers.mp4.mp4?alt=media&token=47b55862-0385-480c-ac3c-30ace0b7c76c ",
     },
     {
         id: "ca22",
