1f01f909d302e2255061f9d5aeff052962dd2e96
IT21282454 <it21282454@my.sliit.lk>
2025-03-22 14:46:47 +0530

    Create FailedAlert.tsx

---
+++ frontend/app/constants/FailedAlert.tsx
@@ -0,0 +1,98 @@
+import { View, Text, Modal, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import LottieView from "lottie-react-native";
+import { useTheme } from "../ThemeContext";
+import FontLoader from "../../FontLoader";
+
+interface alert {
+    failed: boolean;
+    setFailed: (value: boolean) => void;
+    lottie: any;
+    title: string;
+    text: string;
+}
+
+const FailedAlert: React.FC<alert> = ({
+    failed,
+    setFailed,
+    lottie,
+    title,
+    text,
+}) => {
+    const { theme } = useTheme();
+
+    return (
+        <FontLoader>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={failed}
+                onRequestClose={() => setFailed(false)}
+            >
+                <TouchableOpacity
+                    style={[
+                        styles.modalContainer,
+                        { backgroundColor: theme.loadingModalBg },
+                    ]}
+                    onPress={() => {
+                        setFailed(false);
+                    }}
+                    activeOpacity={1}
+                >
+                    <View
+                        style={[
+                            styles.modalView,
+                            {
+                                backgroundColor: theme.loadingModalBackground,
+                            },
+                        ]}
+                    >
+                        <LottieView
+                            source={lottie}
+                            autoPlay
+                            loop
+                            style={styles.animation}
+                        />
+                        <Text
+                            style={[styles.title, { color: theme.textPrimary }]}
+                        >
+                            {title}
+                        </Text>
+                        <Text style={[styles.text, { color: theme.dimText }]}>
+                            {text}
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </Modal>
+        </FontLoader>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalView: {
+        width: "75%",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 55,
+        borderRadius: 10,
+    },
+    animation: {
+        width: 180,
+        height: 180,
+    },
+    title: {
+        fontFamily: "robotoBold",
+        fontSize: 16,
+    },
+    text: {
+        fontSize: 13,
+        marginTop: 10,
+    },
+});
+
+export default FailedAlert;
